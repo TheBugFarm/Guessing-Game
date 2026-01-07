8369c79c31b1a9ca71079a5e2b16b87fca73e3e9
TheBugFarm <its.navneet.31@gmail.com>
2026-01-07 23:48:03 +0530

    Added a secret number and used rand in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use std::io;
+use rand::Rng;
 fn main() {
 
     println!("Welcome to The Number Guessing Game ");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess a number between 1 and 100 ");
+
     let mut guess = String::new();
+
+    println!("The secret number is {secret_number}");
+
     io::stdin().read_line(&mut guess).expect("Failed to read user input");
+
     println!("You guessed {guess}");
+
 }
